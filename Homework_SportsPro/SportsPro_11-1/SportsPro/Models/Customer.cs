3d7da5054b97b3f91e820f778cce0c7f78123526
loudiamond13 <loudiamond_loyloy@insideranken.org>
2023-11-04 07:46:04 -0500

    SportsPro_11-1

---
+++ Homework_SportsPro/SportsPro_11-1/SportsPro/Models/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace SportsPro.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+
+
+      
+
+       
+        [Display(Name = "First Name")]
+        [StringLength(51, MinimumLength = 1, ErrorMessage = "Length must consist 1-25 character.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        public string State { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Country")]
+        public string CountryID { get; set; } = string.Empty;
+
+        //[ValidateNever]
+        public Country? Country { get; set; }
+
+
+        [ValidateNever]
+        public string Phone { get; set; } = "";
+
+        [ValidateNever]
+        public string Email { get; set; } = "";
+
+
+
+        public string FullName => FirstName + " " + LastName;   // read-only property
+
+    }
+}
---
+++ Homework_SportsPro/SportsPro_11-1/SportsPro/Models/Validations/Validate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace SportsPro.Models.Validations
+{
+    public static class Validate
+    {
+        
+        public static string CheckSelectedTech(Technician technician)
+        {
+            return (technician?.TechnicianID == null || technician.TechnicianID == 0) ? "Please Select A Technician" : "";
+        }
+
+
+       
+
+        public static string CheckPhoneFormat(string checkFormat)
+        {
+            string format = @"\d{3}-\d{3}-\d{4}";
+            string message = string.Empty;
+            if (string.IsNullOrEmpty(checkFormat) || Regex.IsMatch(checkFormat, format))
+            {
+                message = "";
+            }
+            else 
+            {
+                message = "Phone Number Must Be In 000-000-0000 Format.";
+            }
+
+            return message;
+        }
+    }
+}
